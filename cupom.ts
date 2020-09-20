482db8c06662e75c04b72db4ff8178e82f8140b8
josehailton <71135811+josehailton@users.noreply.github.com>
2020-09-20 11:57:27 -0300

    Update cupom.ts
    Implementei os dados a serem impressos quando a função é chamada.

--- cupom.ts
+++ cupom.ts
@@ -1,6 +1,11 @@
 function imprime_dados_loja() {
-  // Implemente aqui
-  return "";
+  console.log(nome_loja)
+  console.log(`${logradouro}, ${numero} ${complemento}`);
+  console.log(`${bairro} - ${municipio} - ${estado}`);
+  console.log(`CEP: ${cep}, Tel ${telefone}`);
+  console.log(observacao);
+  console.log(`CNPJ: ${cnpj}`);
+  console.log(`IE: ${inscricao_estadual}`);
 }
 
 module.exports = imprime_dados_loja;
